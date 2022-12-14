eec41b6171a9a6d7dfde020f254a55898cbf1b82
Michelle <mntomlin1@gmail.com>
2022-12-14 11:33:04 -0500

    updated URL

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,11 +5,15 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL =
+/*const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+*/
+
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "https://mt-restaurant-reservation-back-end.onrender.com";
 
 /**
- * Defines the default headers for these functions to work with `json-server`
+* Defines the default headers for these functions to work with `json-server`
  */
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
